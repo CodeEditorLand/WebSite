8356a39256069d36b1fdcb25ccb2f73d0798bcad
Nikola Hristov <nikola@nikolahristov.tech>
2023-12-14 09:21:09 +0200

    squash!

--- Source/Script/Firebase.ts
+++ Source/Script/Firebase.ts
@@ -1,10 +1,11 @@
 export default (await import("firebase/app")).initializeApp({
-	apiKey: "", // TODO Replace with your API key
-	appId: "", // TODO Replace with your app ID
-	authDomain: "", // TODO Replace with your auth domain
-	databaseURL: "", // TODO Replace with your database URL
-	measurementId: "", // TODO Replace with your measurement ID
-	messagingSenderId: "", // TODO Replace with your messaging sender ID
-	projectId: "", // TODO Replace with your project ID
-	storageBucket: "", // TODO Replace with your storage bucket
+	apiKey: "AIzaSyCT9aEJsoE6hbA5dOYwg1zYmJLRVrLTdP8",
+	appId: "1:693091182396:web:f8af7a0361f0b3d00ba039",
+	authDomain: "code-editor-land.firebaseapp.com",
+	databaseURL:
+		"https://code-editor-land-default-rtdb.europe-west1.firebasedatabase.app/",
+	measurementId: "G-M9TTCEF4GL",
+	messagingSenderId: "693091182396",
+	projectId: "code-editor-land",
+	storageBucket: "code-editor-land.appspot.com",
 });
