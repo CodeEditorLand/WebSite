0f9c0d7e00102a8c02d13c65d10b59db00979e31
Nikola R. Hristov <Nikola@playform.cloud>
2024-03-28 03:15:02 +0200

    squash!

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # 🗺️ [Website]
 
-[Website]: https://Land.Playform.Cloud
+[Website]: https://Land.playform.cloud
--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,7 @@ export default (await import("astro/config")).defineConfig({
 	srcDir: "./Source",
 	publicDir: "./Public",
 	outDir: "./Target",
-	site: "https://Land.Playform.Cloud/",
+	site: "https://Land.playform.cloud/",
 	compressHTML: true,
 	integrations: [
 		import.meta.env.MODE === "production"
